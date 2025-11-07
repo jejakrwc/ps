515da933ae1a17fcb4eddee0e20fe04ca85b0fb3
jejakrwc <37380124+jejakrwc@users.noreply.github.com>
2025-11-07 10:28:56 +0700

    data-booking.js

--- data-booking.js
+++ data-booking.js
@@ -22,8 +22,8 @@ const bookedData = [
   { nama: "KOSONG", date: "2025-11-01", start: "19:18", duration: 1.5, konsol: "PS5", room: "Reguler 3", order_via: "Datang Langsung", operator: "RIZKI" },
   { nama: "KOSONG", date: "2025-11-01", start: "00:00", duration: 0, konsol: "PS5", room: "Reguler 4", order_via: "Datang Langsung", operator: "RIZKI" },
   { nama: "KOSONG", date: "2025-11-01", start: "13:43", duration: 2, konsol: "PS4", room: "Reguler 5", order_via: "Datang Langsung", operator: "RIZKI" },
-  { nama: "KOSONG", date: "2025-11-01", start: "10:35", duration: 1, konsol: "PS4", room: "Reguler 6", order_via: "Datang Langsung", operator: "RIZKI" },
-  { nama: "KOSONG", date: "2025-11-01", start: "00:00", duration: 0, konsol: "PS4", room: "Reguler 7", order_via: "Datang Langsung", operator: "RIZKI" },
+  { nama: "KOSONG", date: "2025-11-07", start: "10:00", duration: 2, konsol: "PS4", room: "Reguler 6", order_via: "Datang Langsung", operator: "RIZKI" },
+  { nama: "KOSONG", date: "2025-11-07", start: "10:28", duration: 1, konsol: "PS4", room: "Reguler 7", order_via: "Datang Langsung", operator: "RIZKI" },
   { nama: "KOSONG", date: "2025-11-07", start: "09:30", duration: 2, konsol: "PS4", room: "Reguler 8", order_via: "Datang Langsung", operator: "RIZKI" },
   { nama: "KOSONG", date: "2025-11-01", start: "00:00", duration: 0, konsol: "PS5", room: "Smoking 9", order_via: "Datang Langsung", operator: "RIZKI" },
   { nama: "SMA", date: "2025-11-01", start: "11:30", duration: 1, konsol: "PS4", room: "Smoking 10", order_via: "Datang Langsung", operator: "RIZKI" },
@@ -101,4 +101,5 @@ const roomsByConsole = {
 if (typeof window !== "undefined") {
   window.bookedData = bookedData;
 }
+
 
